f4d6f6ebfa3d7c6ff9bd8add538340c135b5050b
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-03-19 12:56:07 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-# from tensorflow import keras
-# import numpy as np
-
-# def predict():
-
-#     model = keras.models.load_model("lstm_model.h5")
-
-#     pred = model.predict(...)
-
-#     return pred
-
 import numpy as np
 
 def predict_numbers():
