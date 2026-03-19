f0b5cf5fd6aa548303d52068a1e4b0e41375e310
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-03-19 12:36:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,15 +4,36 @@
 # In[ ]:
 
 
+# from flask import Flask, render_template
+# from predict import predict
+
+# app = Flask(__name__)
+
+# @app.route("/predict")
+# def run_prediction():
+
+#     numbers = predict()
+
+#     return render_template("prediction.html", numbers=numbers)
+
+
+# In[ ]:
+
+
 from flask import Flask, render_template
-from predict import predict
+from predict import predict_numbers
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/predict")
 def run_prediction():
-
-    numbers = predict()
-
+    numbers = predict_numbers()
     return render_template("prediction.html", numbers=numbers)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
