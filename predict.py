908d9684fb16fbebe8894e4ffd4662de19efd3f9
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-03-19 12:54:51 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,14 +4,18 @@
 # In[ ]:
 
 
-from tensorflow import keras
+# from tensorflow import keras
+# import numpy as np
+
+# def predict():
+
+#     model = keras.models.load_model("lstm_model.h5")
+
+#     pred = model.predict(...)
+
+#     return pred
+
 import numpy as np
 
-def predict():
-
-    model = keras.models.load_model("lstm_model.h5")
-
-    pred = model.predict(...)
-
-    return pred
-
+def predict_numbers():
+    return list(np.random.choice(range(1, 50), 7, replace=False))
